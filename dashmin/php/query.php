fc97021035cd425538fe152b7ebf6e099460fa61
Sharjeel khan <iamkhansharjeel@gmail.com>
2024-12-03 11:22:46 +0500

    data submitted

--- dashmin/php/query.php
+++ dashmin/php/query.php
@@ -1,3 +1,25 @@
 <?php
 include "connection.php";
+
+//add Category 
+
+if(isset($_POST['addCategory'])){
+    $name=$_POST['catName'];
+    $imagename=$_FILES['catImage']['name'];
+    $imageobject=$_FILES['catImage']['tmp_name'];
+    $extension= pathinfo($imagename,PATHINFO_EXTENSION);
+    $pathdirectory="img/categories/".$imagename;
+    if($extension == "jpg" || $extension == "png" || $extension == "jpeg" || $extension == "webp"){
+        if(move_uploaded_file($imageobject,$pathdirectory)){
+            //query Prepration
+            $query=$pdo ->prepare("insert into categories(name,image) values (:pn,:pimg)");
+            $query->bindParam("pn",$name);
+            $query->bindParam("pimg",$imagename);
+            $query->execute();
+            echo "<script>alert('Data Submitted Successfully')</script>";
+        }
+    }else{
+        echo "<script>alert('Invalid file type use only jpg, jpeg, png or webp')</script>";
+    }
+}
 ?>
